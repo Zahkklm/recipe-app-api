403f89ce3ab1222e086db5a55544ad6e82f3d9c6
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-02-26 19:01:38 +0300

    Create core app with race condition for database initialization

---
+++ app/core/management/commands/__init__.py
@@ -0,0 +1,9 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django management command to wait for database service initialization."""
+
+    def handle(self, *args, **options):
+        pass
+
+    
