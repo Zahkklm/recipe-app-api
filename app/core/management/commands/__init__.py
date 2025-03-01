75b4215447532a13ff8bd75dda1dc8447b56c79b
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-03-02 00:40:57 +0300

    Fix linting errors to comply with PEP8 (remove unused imports, end of line errors)

--- app/core/management/commands/__init__.py
+++ app/core/management/commands/__init__.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """Django management command to wait for database service initialization."""
+    """
+    Django management command to wait for database service initialization.
+    """
 
     def handle(self, *args, **options):
         pass
-
-    
