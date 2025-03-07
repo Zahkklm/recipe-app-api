7aec120f23b9a76a7acd287e6e42482a41cbd07e
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-03-07 03:44:27 +0300

    Create unit test for testing Recipe model
    def test_create_recipe(self):
            """Test creating a recipe is successful."""

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,6 +3,9 @@ Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
+from decimal import Decimal
+
+from core import models
 
 
 class ModelTest(TestCase):
@@ -48,3 +51,19 @@ class ModelTest(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a recipe is successful."""
+        user = get_user_model().objects.create_user(
+            'recipetest@example.com',
+            'recipetestpass123',
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title='sample recipe',
+            description='sample description for recipe',
+            price=Decimal('12.50'),
+            time_minutes=5,
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
