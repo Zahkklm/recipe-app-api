fc083b10a42d52ab59f27d9fca2a87e2471971f5
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-03-08 21:45:04 +0300

    Modify app settings with 'COMPONENT_SPLIT_REQUEST': True which enables the splitting of multi-component images (like RGB, CMYK) into their separate components when processing requests.

--- app/app/settings.py
+++ app/app/settings.py
@@ -147,5 +147,6 @@ SPECTACULAR_SETTINGS = {
     'TITLE': 'Recipe APP API',
     'DESCRIPTION': 'Fully Dockerized Django Recipe App API',
     'VERSION': '1.0.0',
-    'SERVE_INCLUDE_SCHEMA': False,
+    'SERVE_INCLUDE_SCHEMA': False,    
+    'COMPONENT_SPLIT_REQUEST': True,
 }
