858045627b261a4c023a99b1c189ec7c574789a7
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-03-08 20:34:55 +0300

    Implement Ingredient model, ForeignKey=user model

--- app/core/models.py
+++ app/core/models.py
@@ -54,9 +54,22 @@ class Recipe(models.Model):
     price = models.DecimalField(decimal_places=2, max_digits=5)
     link = models.CharField(max_length=255, blank=True)
     tags = models.ManyToManyField('Tag')
+    ingredients = models.ManyToManyField('Ingredient')
 
     def __str__(self):
         return self.title
+
+
+class Ingredient(models.Model):
+    """Ingredient for recipes."""
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return self.name
 
 
 class Tag(models.Model):
